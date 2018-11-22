48593e455917b521c522be997e02ce022819e258
Ikeda <MIYAMASA362@yahoo.co.jp>
2018-11-22 14:21:50 +0900

    Sourceを変更

--- Source/main.cpp
+++ Source/main.cpp
@@ -46,56 +46,6 @@ static CAirplane Airplane(
 	)
 );
 
-/*
-static CAirplane Airplane_Sub1(
-	&Transform(
-		{ 2.0f,2.7f,5.0f },
-		{ 0.005f,0.005f,0.005f },
-		{ 0.0f,0.0f,D3DXToRadian(40) }
-	)
-);
-
-static CAirplane Airplane_Sub2(
-	&Transform(
-		{ -2.4f,2.7f,5.0f },
-		{ 0.005f,0.005f,0.005f },
-		{ 0.0f,0.0f,D3DXToRadian(60) }
-	)
-);
-
-static GameObject Tank(
-	&Transform(
-		{3.0f,0.75f,-2.0f},
-		{1.5f,1.5f,1.5f}
-	)
-);
-
-static GameObject Tree1(
-	&Transform(
-		{3.8f,0.0f,0.0f},
-		{3.0f,3.0f,3.0f}
-	)
-);
-
-static GameObject Tree2(
-	&Transform(
-		{ 5.0f,0.0f,3.0f },
-		{ 3.0f,3.0f,3.0f }
-	)
-);
-static GameObject Tree3(
-	&Transform(
-		{ 6.0f,0.0f,1.0f },
-		{ 3.0f,3.0f,3.0f }
-	)
-);
-
-
-static GameObject Airplane_Obj1(
-	&Transform({ 0.0f,0.6f,5.0f },{0.05f, 0.05f, 0.05f}, {0.0f,0.0f,0.0f})
-);
-*/
-
 //=============================================================
 //	èâä˙âªèàóù
 //=============================================================
@@ -104,22 +54,14 @@ void Main_Initialize(void)
 
 	XModel::Load();
 
-	g_Camera.Set_Main();
-	//Airplane.Boarding();
+	//g_Camera.Set_Main();
+	Airplane.Boarding();
 	Billboard_Initialize();
 
 	MeshField_Ground_Initialize();
 	MeshCylinder_Initialize();
 	MeshSkyDome_Initialize();
 	//Grid_Initialize();
-
-	/*
-	Airplane_Obj1.render.SetMesh(new CXModelName(XModel::AIRPLANE_MODEL));
-	Tank.render.SetMesh(new CXModelName(XModel::TANK));
-	Tree1.render.SetMesh(new CXModelName(XModel::TREE));
-	Tree2.render.SetMesh(new CXModelName(XModel::TREE));
-	Tree3.render.SetMesh(new CXModelName(XModel::TREE));
-	*/
 	
 }
 
@@ -136,31 +78,12 @@ void Main_UpdateBegin(void)
 	
 	GameObject::g_Update();
 
-	if(Keyboard_IsPress(DIK_1))
-	{
-		Airplane.transform.rotation.z += D3DXToRadian(1);
-	}
-
-	if (Keyboard_IsPress(DIK_2))
-	{
-		Airplane.transform.rotation.x += D3DXToRadian(1);
-	}
-
-	if (Keyboard_IsPress(DIK_3))
-	{
-		Airplane.transform.rotation.y += D3DXToRadian(1);
-	}
-
-	if(Keyboard_IsPress(DIK_U))
-	{
-		Airplane.transform.position -= Airplane.transform.forward * 0.1f;
-	}
-
-	g_Camera.transform = Airplane.transform;
+	//g_Camera.transform = Airplane.transform;
 
 	//	ÉJÉÅÉâÇÃå¸Ç´Ç≈à⁄ìÆÇ∑ÇÈÇ‚Ç¬
 	//----------------------------------------------
 	g_Camera.Update();
+	
 	
 }
 
@@ -175,6 +98,7 @@ void Main_Render(void)
 	//Grid_Render();
 	MeshSkyDome_Render(D3DXVECTOR3(0.0f, 0.0f, 0.0f),MeshCylinderTex);
 	MeshField_Ground_Render(D3DXVECTOR3(0.0f,0.0f,0.0f), MeshFieldTex);
+	BillBoard_Create(D3DXVECTOR3(1.0f, 1.0f, 1.0f));
 }
 
 
